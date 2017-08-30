0c2a60da956c3de924e4bab2fc8ed561d2d06bec
Daniel Sullivan <dansull@gmail.com>
2017-08-30 14:20:41 -0500

    circleci

--- make-znapzend.py
+++ make-znapzend.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
-import shlex
-import tempfile
-import re
 
-from contextlib import contextmanager
+from __future__ import print_function
+
 import os
+import re
+import shlex
 import shutil
-from os.path import basename
-from subprocess import check_output
+import sys
+from Queue import Queue
+from contextlib import contextmanager
+from subprocess import check_output, Popen, PIPE
+from threading import Thread
 
 USER_EMAIL = os.environ['USER_EMAIL']
 USER_NAME = os.environ['USER_NAME']
@@ -15,8 +18,7 @@ PPA = os.environ['PPA']
 
 ZZ_URL = 'https://github.com/oetiker/znapzend.git'
 
-ZZD_URL = 'https://github.com/mumblepins/znapzend-debian.git'
-ZZD_BRANCH = 'develop'
+build_dir = 'znapzend-build'
 
 environment = dict(os.environ,
                    DEBEMAIL=USER_EMAIL,
@@ -24,41 +26,125 @@ environment = dict(os.environ,
                    DEB_BUILD_OPTIONS="nocheck"
                    )
 
+RED = "\033[1;31m"
+BLUE = "\033[1;34m"
+CYAN = "\033[1;36m"
+GREEN = "\033[0;32m"
+RESET = "\033[0;0m"
+
+current_branch = check_output(shlex.split('git rev-parse --abbrev-ref HEAD')).strip()
+deploy = True if current_branch == 'master' else False
+
+
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+
 
 @contextmanager
 def cd(newdir):
     prevdir = os.getcwd()
     os.chdir(os.path.expanduser(newdir))
     try:
-        yield
+        yield prevdir, os.getcwd()
     finally:
         os.chdir(prevdir)
+
+
+def reader(pipe, queue, name):
+    try:
+        with pipe:
+            for line in iter(pipe.readline, b''):
+                queue.put((pipe, line, name))
+    finally:
+        queue.put(None)
+
+
+def run_command_iter(string, echo=True, quiet=False, dry_run=False, colored=True, *args, **kwargs):
+    if 'shell' not in kwargs or not kwargs['shell']:
+        cmd = shlex.split(string)
+    else:
+        cmd = string
+    if echo:
+        if dry_run:
+            eprint("Dry run: {}".format(cmd))
+        else:
+            eprint("Running: {}".format(cmd))
+    if dry_run:
+        return
+    process = Popen(cmd, stdout=PIPE, stderr=PIPE, *args, **kwargs)
+    # print process.communicate()
+    # sys.exit()
+    q = Queue()
+    Thread(target=reader, args=[process.stdout, q, 'stdout']).start()
+    Thread(target=reader, args=[process.stderr, q, 'stderr']).start()
+    for _ in range(2):
+        for source, line, name in iter(q.get, None):
+            if not quiet:
+                if name == 'stdout':
+                    yield 'stdout', line
+                else:
+                    if colored:
+                        line = RED + line + RESET
+                    yield 'stderr', line
+                    # sys.stderr.write(RED)
+                    # sys.stderr.write(line)
+                    # sys.stderr.write(RESET)
+
+
+def run_command(*args, **kwargs):
+    for t, ln in run_command_iter(*args, **kwargs):
+        if t == 'stdout':
+            sys.stdout.write(ln)
+        else:
+            sys.stderr.write(ln)
+
+
+def mkdirp(directory):
+    d = os.path.abspath(directory)
+    try:
+        os.makedirs(d, 0o0700)
+    except OSError as e:
+        if e.errno == 17:  # Directory exists
+            os.chown(d, os.getuid(), os.getgid())
+            os.chmod(d, 0o0700)
+        else:
+            raise
+    return d
+
+
+def clean(directory):
+    try:
+        shutil.rmtree(directory)
+    except OSError as e:
+        if e.errno == 2:
+            pass
+        else:
+            raise
 
 
 def clone_and_checkout(url, branch=None, gitdir=None):
     if gitdir is None:
         gitdir = os.path.basename(url).replace('.git', '')
-    _cmd = shlex.split('git clone {} {}'.format(url, gitdir))
-
-    print check_output(_cmd)
+    run_command('git clone {} {}'.format(url, gitdir))
     if branch is not None:
         with cd(gitdir):
-            _cmd = shlex.split('git checkout {}'.format(branch))
-            print check_output(_cmd)
+            run_command('git checkout {}'.format(branch))
     return gitdir
 
 
-tempdir = tempfile.mkdtemp()
-with cd(tempdir):
+run_command('curl -SlL {} | gpg --import'.format(os.environ['SIGN_URI']), echo=False, quiet=True, shell=True)
+
+clean(build_dir)
+build_dir = mkdirp(build_dir)
+
+with cd(build_dir) as (prevdir, curdir):
     zz_dir = clone_and_checkout(ZZ_URL)
-    zzd_dir = clone_and_checkout(ZZD_URL, ZZD_BRANCH)
-    shutil.move(os.path.join(zzd_dir, 'debian'), zz_dir)
-    shutil.rmtree(zzd_dir)
+    shutil.copytree(os.path.join(prevdir, 'debian'), os.path.join(zz_dir, 'debian'))
     zz_dir = os.path.abspath(zz_dir)
 
 with cd(zz_dir):
-    print check_output('./configure')
-    print check_output('make')
+    run_command('./configure')
+    run_command('make')
 
     with open('thirdparty/Makefile.am', 'r') as mkfh:
         mkdata = mkfh.readlines()
@@ -73,18 +159,25 @@ with cd(zz_dir):
                 found_touch = True
 
     os.remove('thirdparty/Makefile')
-    print check_output('automake')
+    run_command('automake')
 
-    cmd = shlex.split("debuild -S")
-    for line in check_output(cmd, env=environment).splitlines():
+    cmd = "debuild --no-tgz-check -S  -p'gpg --no-tty --passphrase {}'".format(os.environ['SIGN_PASSWORD'])
+    for typ, line in run_command_iter(cmd, env=environment, shell=True, echo=False):
         if 'signfile' in line and '.changes' in line:
+            sys.stdout.write(BLUE)
             chngfile = re.search(r'\s+(\S*\.changes)\s+', line).groups()[0]
-        print line
+        if typ == 'stderr':
+            sys.stderr.write(line)
+        else:
+            sys.stdout.write(line)
+        sys.stdout.write(RESET)
 
-with cd(tempdir):
-    cmd = shlex.split('dput -u ppa:{ppaname} {chngfile}'.format(ppaname=PPA, chngfile=chngfile))
-    print cmd
+with cd(build_dir):
+    if deploy:
+        cmd = 'dput -u ppa:{ppaname} {chngfile}'.format(ppaname=PPA, chngfile=chngfile)
+    else:
+        cmd = 'dput -u -s ppa:{ppaname} {chngfile}'.format(ppaname=PPA, chngfile=chngfile)
 
-    print check_output(cmd, env=environment)
+    run_command(cmd, env=environment)
 
-print tempdir
+print(build_dir)
